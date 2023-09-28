210fef884dc4d9469107b6191158351c5639c3b1
Ieuan <ieuan.davies2@gmail.com>
2023-09-29 10:40:14 +1300

    ooops

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ def to_upper(name):
 
 def say_hello(name):
     print(f'Hello, {name}')
-
-if __name__ == '__main__':
-    name = 'Tony'
-    say_hello(name)
-    up = to_upper(name)
-    print(up)
+    if __name__ == '__main__':
+        name = 'Tony'
+        say_hello(name)
+        up = to_upper(name)
+        print(up)
